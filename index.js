2ad9fdbc19f0aaa3dd888396645e033ffa86e878
Ryan Machado <37838624+rymach@users.noreply.github.com>
2019-07-23 21:10:50 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+module.exports = {
+  rules: {
+    'no-console': 'off',
+    '@connexta/connexta/no-absolute-urls': 2,
+  },
+  globals: {
+    "define": "writable",
+  },
+  env: {
+    "browser": true,
+    "node": true,
+    "mocha": true,
+    "es6": true
+  },
+  settings: {
+    react: { version: 'detect' },
+  },
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+    ecmaFeatures: { jsx: true },
+  },
+  plugins: ['react', '@connexta/connexta'],
+  parser: 'babel-eslint',
+}
